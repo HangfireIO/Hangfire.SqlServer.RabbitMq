7738e1fb612bfdc2d99543957ec07178fd8bd3db
Sergey Odinokov <odinserj@gmail.com>
2016-08-19 11:44:19 +0300

    Copy Hangfire.SqlServer.RabbitMq project

---
+++ psake-project.ps1
@@ -0,0 +1,25 @@
+Framework 4.5.1
+Properties {
+    $solution = "Hangfire.SqlServer.RabbitMq.sln"
+}
+
+Include "packages\Hangfire.Build.0.2.5\tools\psake-common.ps1"
+
+Task Default -Depends Collect
+
+Task Test -Depends Compile -Description "Run unit and integration tests under OpenCover." {
+}
+
+Task Merge -Depends Test -Description "Run ILMerge /internalize to merge assemblies." {
+}
+
+Task Collect -Depends Merge -Description "Copy all artifacts to the build folder." {
+    Collect-Assembly "Hangfire.SqlServer.RabbitMq" "Net45"
+}
+
+Task Pack -Depends Collect -Description "Create NuGet packages and archive files." {
+    $version = Get-PackageVersion
+    
+    Create-Archive "Hangfire.SqlServer.RabbitMq-$version"
+    Create-Package "Hangfire.SqlServer.RabbitMq" $version
+}
